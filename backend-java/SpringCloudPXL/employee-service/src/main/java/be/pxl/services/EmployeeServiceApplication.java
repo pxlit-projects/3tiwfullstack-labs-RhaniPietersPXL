1fc38d789230b13f62e09a6398b97ade7f7c1fe2
RhaniPietersPXL <rhani.pieters@student.pxl.be>
2024-09-19 14:50:41 +0200

    back-end eerste project

---
+++ backend-java/SpringCloudPXL/employee-service/src/main/java/be/pxl/services/EmployeeServiceApplication.java
@@ -0,0 +1,11 @@
+package be.pxl.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(be.pxl.services.EmployeeServiceApplication.class, args);
+    }
+}
---
+++ backend-java/SpringCloudPXL/employee-service/src/main/java/be/pxl/services/controller/EmployeeController.java
@@ -0,0 +1,23 @@
+package be.pxl.services.controller;
+
+import be.pxl.services.services.IEmployeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employee")
+@RequiredArgsConstructor
+public class EmployeeController {
+
+    private final IEmployeeService employeeService;
+
+    @GetMapping
+    public ResponseEntity getEmployees() {
+        return new ResponseEntity(employeeService.getAllEmployees(), HttpStatus.OK);
+    }
+}
