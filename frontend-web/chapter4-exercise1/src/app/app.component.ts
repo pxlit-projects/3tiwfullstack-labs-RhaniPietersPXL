57161daa5146650c55afb4295e8ab6c6ad0792aa
RhaniPietersPXL <rhani.pieters@student.pxl.be>
2024-10-02 15:45:23 +0200

    start exercise 2

--- frontend-web/chapter4-exercise1/src/app/app.component.ts
+++ frontend-web/chapter4-exercise1/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {FormsModule} from "@angular/forms";
+import {CalculatorComponent} from "./calculator/calculator.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, FormsModule, CalculatorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
