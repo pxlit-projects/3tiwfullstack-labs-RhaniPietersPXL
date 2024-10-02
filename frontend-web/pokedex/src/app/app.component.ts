c729dcd80702ab37eb4f7d0381d747a9b0184a96
RhaniPietersPXL <rhani.pieters@student.pxl.be>
2024-10-02 17:23:22 +0200

    chapter 4 exercise 3

--- frontend-web/pokedex/src/app/app.component.ts
+++ frontend-web/pokedex/src/app/app.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FormsModule} from "@angular/forms";
+import {Pokemon} from "./pokemon";
+import {PokemonItemComponent} from "./pokemon-item/pokemon-item.component";
+import {NgClass, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, PokemonItemComponent, NgClass, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pokedex';
+  selectedPokemon: Pokemon | null = null;
+  pokemons!: Pokemon[];
+
+  selectPokemon(pokemon: Pokemon): void {
+    this.selectedPokemon = pokemon;
+  }
+
+  ngOnInit() {
+    this.pokemons = [
+      new Pokemon(1, "Bulbasaur", "grass", "1.png"),
+      new Pokemon(2, "Ivysaur", "grass", "2.png"),
+      new Pokemon(3, "Venusaur", "grass", "3.png"),
+      new Pokemon(4, "Charmander", "fire", "4.png"),
+      new Pokemon(5, "Charmeleon", "fire", "5.png"),
+      new Pokemon(6, "Charizard", "fire", "6.png"),
+      new Pokemon(7, "Squirtle", "water", "7.png"),
+      new Pokemon(8, "Wartortle", "water", "8.png"),
+      new Pokemon(9, "Blastoise", "water", "9.png")
+    ];
+  }
 }
--- frontend-web/pokedex/src/app/pokemon-item/pokemon-item.component.ts
+++ frontend-web/pokedex/src/app/pokemon-item/pokemon-item.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {Pokemon} from "../pokemon";
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-pokemon',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './pokemon-item.component.html',
   styleUrl: './pokemon-item.component.css'
 })
 export class PokemonItemComponent {
-
+  @Input() pokemon!: Pokemon;
+  @Input() isSelected!: boolean;
 }
