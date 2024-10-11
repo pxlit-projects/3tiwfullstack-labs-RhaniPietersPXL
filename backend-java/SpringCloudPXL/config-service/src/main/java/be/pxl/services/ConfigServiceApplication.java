6a4c86866000f09d4d174f57fc956fad3252e52c
RhaniPietersPXL <rhani.pieters@student.pxl.be>
2024-10-11 16:40:23 +0200

    start lab 6

--- backend-java/SpringCloudPXL/config-service/src/main/java/be/pxl/services/ConfigServiceApplication.java
+++ backend-java/SpringCloudPXL/config-service/src/main/java/be/pxl/services/ConfigServiceApplication.java
@@ -1,14 +1,13 @@
 package be.pxl.services;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServiceApplication {
-
     public static void main(String[] args) {
-        new SpringApplicationBuilder(ConfigServiceApplication.class).run(args);
+        SpringApplication.run(ConfigServiceApplication.class, args);
     }
 }
