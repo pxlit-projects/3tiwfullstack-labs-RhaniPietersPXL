3ac54d0d14fb5db9a416f1fa93ff370b7be27af3
RhaniPietersPXL <rhani.pieters@student.pxl.be>
2024-10-24 14:01:29 +0200

    exercise 1 - 3 lab 5

---
+++ frontend-web/pokedex - chapter6/src/app/pokemon-service.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import {Pokemon} from "./pokemon";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokemonServiceService {
+
+  constructor() { }
+
+
+  private _pokemons: Pokemon[] = [
+    new Pokemon(1, "Bulbasaur", "grass", "1.png"),
+    new Pokemon(2, "Ivysaur", "grass", "2.png"),
+    new Pokemon(3, "Venusaur", "grass", "3.png"),
+    new Pokemon(4, "Charmander", "fire", "4.png"),
+    new Pokemon(5, "Charmeleon", "fire", "5.png"),
+    new Pokemon(6, "Charizard", "fire", "6.png"),
+    new Pokemon(7, "Squirtle", "water", "7.png"),
+  ];
+
+
+  getPokemons(): Pokemon[] {
+    return this._pokemons;
+  }
+
+  addPokemon(pokemon: Pokemon): void {
+    this._pokemons.push(pokemon);
+  }
+}
