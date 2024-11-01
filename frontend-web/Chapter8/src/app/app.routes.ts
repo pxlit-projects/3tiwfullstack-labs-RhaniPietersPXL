a25249160efed90078b597ca74b0a7d4e096658d
RhaniPietersPXL <rhani.pieters@student.pxl.be>
2024-11-01 16:57:34 +0100

    start chapter 8 execise 1

--- frontend-web/Chapter8/src/app/app.routes.ts
+++ frontend-web/Chapter8/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./core/home/home.component";
+import {AboutComponent} from "./core/about/about.component";
+import {ContactComponent} from "./core/contact/contact.component";
+import {PageNotFoundComponent} from "./core/page-not-found/page-not-found.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'contact', component: ContactComponent},
+  { path: '**', component: PageNotFoundComponent },    // wildcard route for 404
+];
